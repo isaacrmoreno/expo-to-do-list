943ff046440b65153f9b05d43aec45d3c097c472
Isaac Moreno <isaacmoreno@Isaacs-MacBook-Air.local>
2021-12-11 11:53:26 -0800

    :construction: add User.js and src directory

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
-// Import the functions you need from the SDKs you need
 import * as firebase from 'firebase';
+import { getFirestore } from "firebase/firestore"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
---
+++ src/components/User.js
@@ -0,0 +1,25 @@
+import { collection, addDoc } from "firebase/firestore"; 
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+  });
+
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
