49743af2b03a519d0c40a65e7125f9b1207c4452
Isaac Moreno <isaacmoreno@Isaacs-Air.askey.com>
2022-05-12 16:49:50 -0700

    add github webBrowswer link

--- apps/mobile/src/components/LogOutButton.tsx
+++ apps/mobile/src/components/LogOutButton.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack/'
+import * as WebBrowser from 'expo-web-browser'
+import { AntDesign } from '@expo/vector-icons'
 import { auth } from '../../firebase'
 import { signOut } from 'firebase/auth'
 import Constants from 'expo-constants'
@@ -21,6 +23,10 @@ const LogOutButton = () => {
       .catch((error: { message: string }) => alert(error.message))
   }
 
+  const visitGitHub = async () => {
+    WebBrowser.openBrowserAsync('https://github.com/isaacrmoreno')
+  }
+
   return (
     <View style={styles.containerWrapper}>
       {auth?.currentUser === null ? null : (
@@ -28,11 +34,15 @@ const LogOutButton = () => {
           <Text>Logged In: {auth.currentUser?.email}</Text>
         </View>
       )}
+
       <TouchableOpacity onPress={logOut} style={styles.signOutButton}>
         <Text>Sign Out</Text>
       </TouchableOpacity>
-      <View style={styles.version}>
-        <Text>Version - {Constants?.manifest?.version}</Text>
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={visitGitHub}>
+          <AntDesign name='github' size={30} color='black' />
+        </TouchableOpacity>
+        <Text> - Version - {Constants?.manifest?.version}</Text>
       </View>
     </View>
   )
@@ -60,13 +70,28 @@ const styles = StyleSheet.create({
     height: 30,
     width: '100%',
   },
+  githubButton: {
+    position: 'absolute',
+    bottom: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    height: 30,
+    width: '100%',
+  },
   auth: {
     position: 'absolute',
     bottom: 100,
   },
-  version: {
+  footer: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingHorizontal: 30,
+    alignItems: 'center',
     position: 'absolute',
-    opacity: 80,
-    bottom: 30,
+    bottom: 20,
   },
 })
