948edd1971140b73b913ce06e5773119ecaabfed
Isaac Moreno <isaacmoreno@Isaacs-MacBook-Air.local>
2022-05-26 15:31:58 -0700

    refactor taskScreen by adding LoginSignUpButton component and updated variable names

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -4,7 +4,7 @@ import HomeScreen from './src/screens/HomeScreen'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Toast from 'react-native-toast-message'
-import MyDrawer from './src/navigation/Drawer'
+import MenuDrawer from './src/navigation/Drawer'
 
 const Stack = createNativeStackNavigator()
 
@@ -16,7 +16,7 @@ export default function App() {
         <Stack.Screen
           options={{ headerShown: false, title: 'Drawer' }}
           name='Drawer'
-          component={MyDrawer}
+          component={MenuDrawer}
         />
         <Stack.Screen
           options={{ headerShown: false, title: 'Home' }}
--- apps/mobile/src/components/ScreenTitle.tsx
+++ apps/mobile/src/components/ScreenTitle.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import tw from 'twrnc'
 import { useColorScheme } from 'react-native'
 import { Text, View } from 'react-native'
-import { HeaderProps } from '../types/index'
+import { ScreenTitleProps } from '../types/index'
 
-const Header: React.FC<HeaderProps> = ({ ScreenTitle }) => {
+const ScreenTitle: React.FC<ScreenTitleProps> = ({ title }) => {
   const colorScheme = useColorScheme()
   return (
     <View style={tw`flex-1`}>
@@ -13,10 +13,10 @@ const Header: React.FC<HeaderProps> = ({ ScreenTitle }) => {
           tw`top-14 font-bold text-2xl`,
           colorScheme === 'dark' ? tw`text-white` : tw`text-black`,
         ]}>
-        {ScreenTitle}
+        {title}
       </Text>
     </View>
   )
 }
 
-export default Header
+export default ScreenTitle
