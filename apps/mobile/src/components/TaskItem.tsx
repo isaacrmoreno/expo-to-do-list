ef26b742cdc4ed1f038007b1fa828faabf47bf86
Isaac Moreno <isaacmoreno@Isaacs-Air.askey.com>
2022-05-27 15:54:36 -0700

    refactor colorScheme for icons

--- apps/mobile/src/components/TaskItem.tsx
+++ apps/mobile/src/components/TaskItem.tsx
@@ -19,42 +19,23 @@ const TaskItem: React.FC<TaskItemProps> = ({
         colorScheme === 'dark' && tw`bg-neutral-700`,
       ]}>
       <View style={tw`flex-row items-center`}>
-        {colorScheme === 'dark' ? (
-          <Feather
-            style={tw`mr-2`}
-            name='edit'
-            size={24}
-            color='white'
-            onPress={() => editTask(index)}
-          />
-        ) : (
-          <Feather
-            style={tw`mr-2`}
-            name='edit'
-            size={24}
-            color='black'
-            onPress={() => editTask(index)}
-          />
-        )}
+        <Feather
+          style={tw`mr-2`}
+          name='edit'
+          size={24}
+          color={colorScheme === 'dark' ? 'white' : 'black'}
+          onPress={() => editTask(index)}
+        />
         <Text style={[tw`max-w-58`, colorScheme === 'dark' && tw`text-white`]}>
           {text}
         </Text>
       </View>
-      {colorScheme === 'dark' ? (
-        <Feather
-          name='trash-2'
-          size={24}
-          color='white'
-          onPress={() => confirmDeleteAlert(index)}
-        />
-      ) : (
-        <Feather
-          name='trash-2'
-          size={24}
-          color='black'
-          onPress={() => confirmDeleteAlert(index)}
-        />
-      )}
+      <Feather
+        name='trash-2'
+        size={24}
+        color={colorScheme === 'dark' ? 'white' : 'black'}
+        onPress={() => confirmDeleteAlert(index)}
+      />
     </View>
   )
 }
