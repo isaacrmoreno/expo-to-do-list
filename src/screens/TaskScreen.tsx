a4f01350eb38b85a52a9c5af0e088e465bed1ea3
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-10 09:55:14 -0700

    WIP: update keyboardAvoidingView

--- src/screens/TaskScreen.tsx
+++ src/screens/TaskScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import {
   KeyboardAvoidingView,
   Platform,
@@ -10,45 +10,45 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-} from 'react-native';
+} from 'react-native'
 
-import TaskItem from '../components/TaskItem';
-import AddTaskButton from '../components/AddTaskButton';
+import TaskItem from '../components/TaskItem'
+import AddTaskButton from '../components/AddTaskButton'
 
 export default function TaskScreen() {
-const [task, setTask] = useState<string | null>('');
-const [currentIndex, setCurrentIndex] = useState<number>(0);
-const [taskItems, setTaskItems] = useState<Array<string | null>>([]);
-const [updateIcon, setUpdateIcon] = useState<Boolean>(false);
+  const [task, setTask] = useState<string | null>('')
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [taskItems, setTaskItems] = useState<Array<string | null>>([])
+  const [updateIcon, setUpdateIcon] = useState<Boolean>(false)
 
-const handleAddTask = () => {
-Keyboard.dismiss();
-setTaskItems([...taskItems, task]);
-setTask(null);
-};
+  const handleAddTask = () => {
+    Keyboard.dismiss()
+    setTaskItems([...taskItems, task])
+    setTask(null)
+  }
 
-const editTask = (index: number) => {
-setUpdateIcon(true);
-const newTask = taskItems[index];
-setTask(newTask);
-setCurrentIndex(index);
-};
+  const editTask = (index: number) => {
+    setUpdateIcon(true)
+    const newTask = taskItems[index]
+    setTask(newTask)
+    setCurrentIndex(index)
+  }
 
-const handleUpdateTask = () => {
-setUpdateIcon(false);
-Keyboard.dismiss();
-let taskItemsCopy = [...taskItems];
-taskItemsCopy.splice(currentIndex, 1, task);
-setTaskItems(taskItemsCopy);
-setTask(null);
-};
+  const handleUpdateTask = () => {
+    setUpdateIcon(false)
+    Keyboard.dismiss()
+    let taskItemsCopy = [...taskItems]
+    taskItemsCopy.splice(currentIndex, 1, task)
+    setTaskItems(taskItemsCopy)
+    setTask(null)
+  }
 
-const completeTask = (index: number) => {
-	let itemsCopy = [...taskItems];
-	itemsCopy.splice(index, 1);
-	setTaskItems(itemsCopy);
-};
-		
+  const completeTask = (index: number) => {
+    let itemsCopy = [...taskItems]
+    itemsCopy.splice(index, 1)
+    setTaskItems(itemsCopy)
+  }
+
   const confirmDeleteAlert = (index: number) =>
     Alert.alert('Delete Task?', 'Are you sure you want to delete this task?', [
       {
@@ -56,7 +56,7 @@ const completeTask = (index: number) => {
         style: 'cancel',
       },
       { text: 'Delete', onPress: () => completeTask(index), style: 'destructive' },
-    ]);
+    ])
 
   return (
     <SafeAreaView style={styles.container}>
@@ -72,7 +72,7 @@ const completeTask = (index: number) => {
                   editTask={editTask}
                 />
               </View>
-            );
+            )
           })}
         </ScrollView>
       </View>
@@ -85,12 +85,19 @@ const completeTask = (index: number) => {
           value={task}
           onChangeText={(text) => setTask(text)}
         />
-        {(updateIcon) 
-					? (<AddTaskButton name='check' size={24} color='black' onPress={handleUpdateTask}/>) 
-					: (<AddTaskButton name='plus' size={24} color='black' onPress={handleAddTask}/>)}
+        {updateIcon ? (
+          <AddTaskButton
+            name='check'
+            size={24}
+            color='black'
+            onPress={handleUpdateTask}
+          />
+        ) : (
+          <AddTaskButton name='plus' size={24} color='black' onPress={handleAddTask} />
+        )}
       </KeyboardAvoidingView>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -98,17 +105,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E8EAED',
     paddingTop: StatusBar.currentHeight,
+    position: 'relative',
   },
   taskWrapper: {
     paddingHorizontal: 20,
   },
   writeTaskWrapper: {
+    // marginBottom: 80,
+    alignItems: 'center',
     position: 'absolute',
+    paddingHorizontal: 20,
     bottom: 50,
-    width: '100%',
+    // width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center',
   },
   input: {
     paddingVertical: 15,
@@ -117,20 +127,10 @@ const styles = StyleSheet.create({
     borderRadius: 60,
     borderColor: '#C0C0C0',
     borderWidth: 1,
-    width: 300,
+    width: '100%',
   },
   scrollView: {
     marginTop: 30,
-    marginBottom: 115,
+    // marginBottom: 115,
   },
-  signOutButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 100,
-    height: 30,
-  },
-});
+})
