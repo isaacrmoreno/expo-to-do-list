750628780923f6eff55d6dafabf2e6409f21a358
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-10 09:55:14 -0700

    update metroConfig + package.json scripts

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -10,7 +10,9 @@ const config = getDefaultConfig(projectRoot);
 
 // 1. Watch all files within the monorepo
 config.watchFolders = [workspaceRoot];
-// 2. Let Metro know where to resolve packages, and in what order
+// 2. Resolve module `idb` from file error
+config.resolver.assetExts.push("cjs");
+// 3. Let Metro know where to resolve packages, and in what order
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
