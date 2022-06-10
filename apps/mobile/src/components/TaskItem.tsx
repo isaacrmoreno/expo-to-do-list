c0d3415d7e225a40964288669a4f48b3775f44fd
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-10 09:55:16 -0700

    remove edit button

--- apps/mobile/src/components/TaskItem.tsx
+++ apps/mobile/src/components/TaskItem.tsx
@@ -1,34 +1,21 @@
 import React from 'react'
 import tw from 'twrnc'
-import { useColorScheme } from 'react-native'
+import { TouchableOpacity, useColorScheme } from 'react-native'
 import { View, Text } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { TaskItemProps } from '../types/index'
 
-const TaskItem: React.FC<TaskItemProps> = ({
-  index,
-  text,
-  confirmDeleteAlert,
-  editTask,
-}) => {
+const TaskItem: React.FC<TaskItemProps> = ({ index, text, confirmDeleteAlert, editTask }) => {
   const colorScheme = useColorScheme()
   return (
-    <View
+    <TouchableOpacity
+      onPress={() => editTask(index)}
       style={[
         tw`bg-white p-4 rounded-lg flex-row items-center justify-between mb-6`,
         colorScheme === 'dark' && tw`bg-neutral-700`,
       ]}>
       <View style={tw`flex-row items-center`}>
-        <Feather
-          style={tw`mr-2`}
-          name='edit'
-          size={24}
-          color={colorScheme === 'dark' ? 'white' : 'black'}
-          onPress={() => editTask(index)}
-        />
-        <Text style={[tw`max-w-58`, colorScheme === 'dark' && tw`text-white`]}>
-          {text}
-        </Text>
+        <Text style={[tw`max-w-58`, colorScheme === 'dark' && tw`text-white`]}>{text}</Text>
       </View>
       <Feather
         name='trash-2'
@@ -36,7 +23,7 @@ const TaskItem: React.FC<TaskItemProps> = ({
         color={colorScheme === 'dark' ? 'white' : 'black'}
         onPress={() => confirmDeleteAlert(index)}
       />
-    </View>
+    </TouchableOpacity>
   )
 }
 
