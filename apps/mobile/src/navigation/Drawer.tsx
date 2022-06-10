cecfe495ba63232886244ae1b129dc9a004dd6fb
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-10 09:55:15 -0700

    WIP: refactor keyboardAvoidingVew on TaskScreen

--- apps/mobile/src/navigation/Drawer.tsx
+++ apps/mobile/src/navigation/Drawer.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import LogOutButton from '../components/LogOutButton';
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import LogOutButton from '../components/LogOutButton'
 import TaskScreen from '../screens/TaskScreen'
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator()
 
 const MyDrawer: React.FC = () => {
   return (
-    <Drawer.Navigator
-		useLegacyImplementation
-		drawerContent={() => <LogOutButton /> }>
-			<Drawer.Screen name="Task" component={TaskScreen} />
+    <Drawer.Navigator useLegacyImplementation drawerContent={() => <LogOutButton />}>
+      <Drawer.Screen name='Task' component={TaskScreen} />
     </Drawer.Navigator>
-  );
+  )
 }
 
 export default MyDrawer
