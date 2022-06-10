68c72b55fd391867a5c3a2d85bd39c6669bcfce8
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-10 09:55:16 -0700

    update signOutButton name to menu

--- apps/mobile/src/navigation/MenuDrawer.tsx
+++ apps/mobile/src/navigation/MenuDrawer.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
-import SignOutButton from '../components/SignOutButton'
+import Menu from '../components/Menu'
 import TaskScreen from '../screens/TaskScreen'
 
 const Drawer = createDrawerNavigator()
 
 const MenuDrawer: React.FC = () => {
   return (
-    <Drawer.Navigator useLegacyImplementation drawerContent={() => <SignOutButton />}>
-      <Drawer.Screen
-        name='Task'
-        component={TaskScreen}
-        options={{ headerShown: false }}
-      />
+    <Drawer.Navigator useLegacyImplementation drawerContent={() => <Menu />}>
+      <Drawer.Screen name='Task' component={TaskScreen} options={{ headerShown: false }} />
     </Drawer.Navigator>
   )
 }
