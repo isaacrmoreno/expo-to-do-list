f261614ed9191082cb854c4ef8c6e8e4a3d159b0
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-10 09:55:15 -0700

    fixed KeyBoardAvoidingView issue - due to drawer hearer

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import HomeScreen from './src/screens/HomeScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Toast from 'react-native-toast-message';
-import MyDrawer from './src/navigation/Drawer';
+import React from 'react'
+import HomeScreen from './src/screens/HomeScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Toast from 'react-native-toast-message'
+import MyDrawer from './src/navigation/Drawer'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName='Drawer'>
-				<Stack.Screen 
-					options={{ headerShown: false, title: 'Drawer' }}
-					name='Drawer'
-					component={MyDrawer}
-				/>
-				<Stack.Screen
-					options={{ headerShown: false, title: 'Home' }}
-					name='Home'
-					component={HomeScreen}
-				/>
-			</Stack.Navigator>
-			<Toast />
-		</NavigationContainer>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Drawer'>
+        <Stack.Screen
+          options={{ headerShown: false, title: 'Drawer' }}
+          name='Drawer'
+          component={MyDrawer}
+        />
+        <Stack.Screen
+          options={{ headerShown: false, title: 'Home' }}
+          name='Home'
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+      <Toast />
+    </NavigationContainer>
+  )
 }
