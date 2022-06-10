341bad1c851a9f30cd0e0dd1b246794bd902e68e
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-10 09:55:15 -0700

    development build built successfully - dark mode not working

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Toast from 'react-native-toast-message'
 import MenuDrawer from './src/navigation/MenuDrawer'
+import 'expo-dev-client'
 
 const Stack = createNativeStackNavigator()
 
