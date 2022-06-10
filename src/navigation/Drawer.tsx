6d651907cf77a5aee00364f73a61485d06afc6dd
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-10 09:55:14 -0700

    WIP: refactor drawer

---
+++ src/navigation/Drawer.tsx
@@ -0,0 +1,13 @@
+import { createDrawerNavigator, DrawerItem } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+const MyDrawer: React.FC = () => {
+  return (
+    <Drawer.Navigator useLegacyImplementation
+		drawerContent={() => <LogOutButton /> }
+		>
+		<Drawer.Screen name="Task" component={TaskButtons} />
+    </Drawer.Navigator>
+  );
+}
