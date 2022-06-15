5943ee289ff6adb4de18b38477bf6c0a8a70577f
isaacrmoreno <ipdxcreative@gmail.com>
2022-06-14 22:11:50 -0700

    add privacy policy

--- apps/mobile/src/components/Menu.tsx
+++ apps/mobile/src/components/Menu.tsx
@@ -8,18 +8,33 @@ import Constants from 'expo-constants'
 const Menu = () => {
   const colorScheme = useColorScheme()
 
+  const visitPrivacyPolicy = () => {
+    WebBrowser.openBrowserAsync(
+      'https://www.privacypolicies.com/live/c567c43b-e1f5-4abe-86a9-35b61c67c4c2'
+    )
+  }
+
   const visitGitHub = async () => {
     WebBrowser.openBrowserAsync('https://github.com/isaacrmoreno')
   }
 
   return (
     <View style={[tw`flex-1 items-center px-5`, colorScheme === 'dark' && tw`bg-neutral-800`]}>
+      <View
+        style={tw`absolute flex-column bottom-16 items-center border-b border-gray-500 w-full pb-2`}>
+        <TouchableOpacity onPress={visitPrivacyPolicy}>
+          <Text style={[tw`font-bold`, colorScheme === 'dark' && tw`text-white`]}>
+            Privacy Policy
+          </Text>
+        </TouchableOpacity>
+      </View>
       <View style={tw`absolute flex-row bottom-6 items-center`}>
         <TouchableOpacity onPress={visitGitHub}>
           <AntDesign name='github' size={30} color={colorScheme === 'dark' ? 'white' : 'black'} />
         </TouchableOpacity>
         <Text style={colorScheme === 'dark' ? tw`text-white` : tw`text-black`}>
-          {'  '}- Version - {Constants?.manifest?.version}
+          {' '}
+          - Version - {Constants?.manifest?.version}
         </Text>
       </View>
     </View>
