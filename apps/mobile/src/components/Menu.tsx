d1151edfe8ffd08969a883304a6a93fd9982bf91
isaacrmoreno <ipdxcreative@gmail.com>
2022-09-12 09:53:06 -0700

    update package scripts for new eas update workflow

--- apps/mobile/src/components/Menu.tsx
+++ apps/mobile/src/components/Menu.tsx
@@ -9,106 +9,106 @@ import useStore from '../store/index'
 import MenuSwitch from './MenuSwitch'
 
 const Menu = () => {
-  const stylized = useStore((state) => state?.stylized)
-  const setStylized = useStore((state) => state?.setStylized)
-  const isMuted = useStore((state) => state?.isMuted)
-  const setIsMuted = useStore((state) => state?.setIsMuted)
+	const stylized = useStore((state) => state?.stylized)
+	const setStylized = useStore((state) => state?.setStylized)
+	const isMuted = useStore((state) => state?.isMuted)
+	const setIsMuted = useStore((state) => state?.setIsMuted)
 
-  const colorScheme = useColorScheme()
-  const itunesItemId = 1630267516
-  const androidPackageName = 'com.expotodolist.prod'
+	const colorScheme = useColorScheme()
+	const itunesItemId = 1630267516
+	const androidPackageName = 'com.expotodolist.prod'
 
-  const showHideStylized = async () => {
-    try {
-      setStylized(!stylized)
-      const jsonValue = JSON.stringify(!stylized)
-      await AsyncStorage.setItem('@stylized', jsonValue)
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const showHideStylized = async () => {
+		try {
+			setStylized(!stylized)
+			const jsonValue = JSON.stringify(!stylized)
+			await AsyncStorage.setItem('@stylized', jsonValue)
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  const playMuteSounds = async () => {
-    try {
-      setIsMuted(!isMuted)
-      const jsonValue = JSON.stringify(!isMuted)
-      await AsyncStorage.setItem('@isMuted', jsonValue)
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const playMuteSounds = async () => {
+		try {
+			setIsMuted(!isMuted)
+			const jsonValue = JSON.stringify(!isMuted)
+			await AsyncStorage.setItem('@isMuted', jsonValue)
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  const onShare = async () => {
-    const result = await Share.share({
-      message: "Download Quail | A minimalist's to do list",
-      url: 'https://quailapp.vercel.app/',
-    })
-    if (result.action === Share.sharedAction) {
-    } else if (result.action === Share.dismissedAction) {
-    }
-  }
+	const onShare = async () => {
+		const result = await Share.share({
+			message: "Download Quail | A minimalist's to do list",
+			url: 'https://quailapp.vercel.app/',
+		})
+		if (result.action === Share.sharedAction) {
+		} else if (result.action === Share.dismissedAction) {
+		}
+	}
 
-  const leaveAppReview = async () => {
-    if (await StoreReview.hasAction()) {
-      StoreReview.requestReview()
-    } else {
-      Platform.OS === 'ios'
-        ? Linking.openURL(
-            `itms-apps://itunes.apple.com/app/viewContentsUserReviews/id${itunesItemId}?action=write-review`
-          )
-        : Linking.openURL(`market://details?id=${androidPackageName}&showAllReviews=true`)
-    }
-  }
+	const leaveAppReview = async () => {
+		if (await StoreReview.hasAction()) {
+			StoreReview.requestReview()
+		} else {
+			Platform.OS === 'ios'
+				? Linking.openURL(
+						`itms-apps://itunes.apple.com/app/viewContentsUserReviews/id${itunesItemId}?action=write-review`
+				  )
+				: Linking.openURL(`https://play.google.com/store/apps/details?id=${androidPackageName}&showAllReviews=true`)
+		}
+	}
 
-  const visitPrivacyPolicy = () => {
-    WebBrowser.openBrowserAsync('https://www.privacypolicies.com/live/c567c43b-e1f5-4abe-86a9-35b61c67c4c2')
-  }
+	const visitPrivacyPolicy = () => {
+		WebBrowser.openBrowserAsync('https://www.privacypolicies.com/live/c567c43b-e1f5-4abe-86a9-35b61c67c4c2')
+	}
 
-  const getMultiple = async () => {
-    let values
-    try {
-      values = await AsyncStorage.multiGet(['@stylized', '@isMuted'])
-      values?.[0][1] !== null && setStylized(JSON.parse(values?.[0][1]))
-      values?.[1][1] !== null && setIsMuted(JSON.parse(values?.[1][1]))
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const getMultiple = async () => {
+		let values
+		try {
+			values = await AsyncStorage.multiGet(['@stylized', '@isMuted'])
+			values?.[0][1] !== null && setStylized(JSON.parse(values?.[0][1]))
+			values?.[1][1] !== null && setIsMuted(JSON.parse(values?.[1][1]))
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  useEffect(() => {
-    getMultiple()
-  }, [])
+	useEffect(() => {
+		getMultiple()
+	}, [])
 
-  return (
-    <View style={[tw`flex-1 items-center px-4`, colorScheme === 'dark' && tw`bg-neutral-800`]}>
-      <TouchableOpacity onPress={onShare} style={tw`absolute top-15 left-4 flex-row`}>
-        <EvilIcons
-          name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'}
-          size={30}
-          color={colorScheme === 'dark' ? 'white' : 'black'}
-        />
-      </TouchableOpacity>
-      <View style={tw`absolute flex-row items-center w-full justify-between bottom-24 pb-2`}>
-        <MenuSwitch text='STYLIZE' onValueChange={showHideStylized} value={stylized} />
-      </View>
-      <View style={tw`absolute flex-row items-center w-full justify-between bottom-14 pb-2`}>
-        <MenuSwitch text='MUTE' onValueChange={playMuteSounds} value={isMuted} />
-      </View>
-      <View
-        style={[
-          tw`absolute flex-row items-center w-full justify-between bottom-6 border-t pt-2`,
-          colorScheme === 'dark' ? tw`border-white` : tw`border-black`,
-        ]}>
-        <TouchableOpacity onPress={visitPrivacyPolicy}>
-          <Text style={[tw`text-xs`, colorScheme === 'dark' ? tw`text-white` : tw`text-black`]}>Privacy policy</Text>
-        </TouchableOpacity>
-        <Text style={colorScheme === 'dark' ? tw`text-white` : tw`text-black`}>|</Text>
-        <TouchableOpacity onPress={leaveAppReview}>
-          <Text style={[tw`text-xs`, colorScheme === 'dark' ? tw`text-white` : tw`text-black`]}>Leave a review</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  )
+	return (
+		<View style={[tw`flex-1 items-center px-4`, colorScheme === 'dark' && tw`bg-neutral-800`]}>
+			<TouchableOpacity onPress={onShare} style={tw`absolute top-15 left-4 flex-row`}>
+				<EvilIcons
+					name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'}
+					size={30}
+					color={colorScheme === 'dark' ? 'white' : 'black'}
+				/>
+			</TouchableOpacity>
+			<View style={tw`absolute flex-row items-center w-full justify-between bottom-24 pb-2`}>
+				<MenuSwitch text='STYLIZE' onValueChange={showHideStylized} value={stylized} />
+			</View>
+			<View style={tw`absolute flex-row items-center w-full justify-between bottom-14 pb-2`}>
+				<MenuSwitch text='MUTE' onValueChange={playMuteSounds} value={isMuted} />
+			</View>
+			<View
+				style={[
+					tw`absolute flex-row items-center w-full justify-between bottom-6 border-t pt-2`,
+					colorScheme === 'dark' ? tw`border-white` : tw`border-black`,
+				]}>
+				<TouchableOpacity onPress={visitPrivacyPolicy}>
+					<Text style={[tw`text-xs`, colorScheme === 'dark' ? tw`text-white` : tw`text-black`]}>Privacy Policy</Text>
+				</TouchableOpacity>
+				<Text style={colorScheme === 'dark' ? tw`text-white` : tw`text-black`}>|</Text>
+				<TouchableOpacity onPress={leaveAppReview}>
+					<Text style={[tw`text-xs`, colorScheme === 'dark' ? tw`text-white` : tw`text-black`]}>Leave a Review</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	)
 }
 
 export default Menu
--- apps/mobile/src/screens/TaskScreen.tsx
+++ apps/mobile/src/screens/TaskScreen.tsx
@@ -2,15 +2,15 @@ import React, { useEffect, useRef, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import tw from 'twrnc'
 import {
-  KeyboardAvoidingView,
-  View,
-  TextInput,
-  Keyboard,
-  ScrollView,
-  useColorScheme,
-  TouchableOpacity,
-  Text,
-  Platform,
+	KeyboardAvoidingView,
+	View,
+	TextInput,
+	Keyboard,
+	ScrollView,
+	useColorScheme,
+	TouchableOpacity,
+	Text,
+	Platform,
 } from 'react-native'
 import { EvilIcons } from '@expo/vector-icons'
 import { LinearGradient } from 'expo-linear-gradient'
@@ -21,212 +21,200 @@ import DrawerToggle from '../components/DrawerToggle'
 import useStore from '../store/index'
 
 export default function TaskScreen() {
-  const [task, setTask] = useState<string>('')
-  const [currentIndex, setCurrentIndex] = useState<number>(0)
-  const [taskList, setTaskList] = useState<object[]>([])
-  const [updateIcon, setUpdateIcon] = useState<boolean>(false)
-  const [isDisabled, setIsDisabled] = useState<boolean>(true)
-  const [sound, setSound] = useState<object>()
+	const [task, setTask] = useState<string>('')
+	const [currentIndex, setCurrentIndex] = useState<number>(0)
+	const [taskList, setTaskList] = useState<object[]>([])
+	const [updateIcon, setUpdateIcon] = useState<boolean>(false)
+	const [isDisabled, setIsDisabled] = useState<boolean>(true)
+	const [sound, setSound] = useState<object>()
 
-  const stylized = useStore((state) => state?.stylized)
-  const isMuted = useStore((state) => state?.isMuted)
+	const stylized = useStore((state) => state?.stylized)
+	const isMuted = useStore((state) => state?.isMuted)
 
-  const inputRef = useRef('')
-  const colorScheme = useColorScheme()
+	const inputRef = useRef('')
+	const colorScheme = useColorScheme()
 
-  const colors = {
-    odd: ['#40c9ff', '#e81cff'],
-    even: ['#f093fb', '#f5576c'],
-  }
+	const colors = {
+		odd: ['#40c9ff', '#e81cff'],
+		even: ['#f093fb', '#f5576c'],
+	}
 
-  const getColor = (index: number) => {
-    let taskColor = null
-    index % 2 === 0 ? (taskColor = colors.odd) : (taskColor = colors.even)
-    return taskColor
-  }
+	const getColor = (index: number) => {
+		let taskColor = null
+		index % 2 === 0 ? (taskColor = colors.odd) : (taskColor = colors.even)
+		return taskColor
+	}
 
-  const handleAddTask = async () => {
-    try {
-      const { sound } = await Audio.Sound.createAsync(require('../../assets/sounds/chime.mp3'), {
-        volume: 0.2,
-        isMuted: isMuted,
-      })
-      setSound(sound)
-      await sound.playAsync()
-      setTaskList([...taskList, { description: task }])
-      taskList.push({ description: task })
-      setTask('')
-      const jsonValue = JSON.stringify(taskList)
-      await AsyncStorage.setItem('@taskList', jsonValue)
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const handleAddTask = async () => {
+		try {
+			const { sound } = await Audio.Sound.createAsync(require('../../assets/sounds/chime.mp3'), {
+				volume: 0.2,
+				isMuted: isMuted,
+			})
+			setSound(sound)
+			await sound.playAsync()
+			setTaskList([...taskList, { description: task }])
+			taskList.push({ description: task })
+			setTask('')
+			const jsonValue = JSON.stringify(taskList)
+			await AsyncStorage.setItem('@taskList', jsonValue)
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  const getTaskList = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem('@taskList')
-      jsonValue !== null && setTaskList(JSON.parse(jsonValue))
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const getTaskList = async () => {
+		try {
+			const jsonValue = await AsyncStorage.getItem('@taskList')
+			jsonValue !== null && setTaskList(JSON.parse(jsonValue))
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  const editTask = (index: number) => {
-    inputRef?.current?.focus()
-    setUpdateIcon(true)
-    const newTask = taskList[index]?.description
-    setTask(newTask)
-    setCurrentIndex(index)
-  }
+	const editTask = (index: number) => {
+		inputRef?.current?.focus()
+		setUpdateIcon(true)
+		const newTask = taskList[index]?.description
+		setTask(newTask)
+		setCurrentIndex(index)
+	}
 
-  const handleUpdateTask = async () => {
-    try {
-      const { sound } = await Audio.Sound.createAsync(require('../../assets/sounds/chime.mp3'), {
-        volume: 0.2,
-        isMuted: isMuted,
-      })
-      setSound(sound)
-      await sound.playAsync()
-      setUpdateIcon(false)
-      Keyboard.dismiss()
-      let updatedTaskList = [...taskList]
-      updatedTaskList.splice(currentIndex, 1, { description: task })
-      setTaskList(updatedTaskList)
-      setTask('')
-      const jsonValue = JSON.stringify(updatedTaskList)
-      await AsyncStorage.setItem('@taskList', jsonValue)
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const handleUpdateTask = async () => {
+		try {
+			const { sound } = await Audio.Sound.createAsync(require('../../assets/sounds/chime.mp3'), {
+				volume: 0.2,
+				isMuted: isMuted,
+			})
+			setSound(sound)
+			await sound.playAsync()
+			setUpdateIcon(false)
+			Keyboard.dismiss()
+			let updatedTaskList = [...taskList]
+			updatedTaskList.splice(currentIndex, 1, { description: task })
+			setTaskList(updatedTaskList)
+			setTask('')
+			const jsonValue = JSON.stringify(updatedTaskList)
+			await AsyncStorage.setItem('@taskList', jsonValue)
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  const completeTask = async (index: number) => {
-    try {
-      const { sound } = await Audio.Sound.createAsync(require('../../assets/sounds/bloop.mp3'), {
-        volume: 0.2,
-        isMuted: isMuted,
-      })
-      setSound(sound)
-      await sound.playAsync()
-      let UpdatedTaskList = [...taskList]
-      UpdatedTaskList.splice(index, 1)
-      setTaskList(UpdatedTaskList)
-      setUpdateIcon(false)
-      await AsyncStorage.setItem('@taskList', JSON.stringify(UpdatedTaskList))
-    } catch (e) {
-      console.log(e)
-    }
-  }
+	const completeTask = async (index: number) => {
+		try {
+			const { sound } = await Audio.Sound.createAsync(require('../../assets/sounds/bloop.mp3'), {
+				volume: 0.2,
+				isMuted: isMuted,
+			})
+			setSound(sound)
+			await sound.playAsync()
+			let UpdatedTaskList = [...taskList]
+			UpdatedTaskList.splice(index, 1)
+			setTaskList(UpdatedTaskList)
+			setUpdateIcon(false)
+			await AsyncStorage.setItem('@taskList', JSON.stringify(UpdatedTaskList))
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-  const changeScreenOrientation = async () => {
-    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT)
-  }
+	const changeScreenOrientation = async () => {
+		await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT)
+	}
 
-  useEffect(() => {
-    task?.length !== 0 ? setIsDisabled(false) : setIsDisabled(true)
-  }, [task])
+	useEffect(() => {
+		task?.length !== 0 ? setIsDisabled(false) : setIsDisabled(true)
+	}, [task])
 
-  useEffect(() => {
-    getTaskList()
-  }, [])
+	useEffect(() => {
+		getTaskList()
+	}, [])
 
-  useEffect(() => {
-    changeScreenOrientation()
-  }, [])
+	useEffect(() => {
+		changeScreenOrientation()
+	}, [])
 
-  useEffect(() => {
-    return sound ? () => sound.unloadAsync() : undefined
-  }, [sound])
+	useEffect(() => {
+		return sound ? () => sound.unloadAsync() : undefined
+	}, [sound])
 
-  return (
-    <View style={[tw`flex-1`, colorScheme === 'dark' ? tw`bg-neutral-800` : tw`bg-slate-100`]}>
-      <DrawerToggle />
-      {(stylized as boolean) ? (
-        <ScrollView style={tw`px-6 mt-4`}>
-          {taskList.map((taskList, index) => {
-            return (
-              <View key={index}>
-                <TouchableOpacity onPress={() => editTask(index)} style={tw`justify-between`}>
-                  <LinearGradient
-                    colors={getColor(index)}
-                    style={tw`p-2 flex-row rounded-lg mb-4 items-center`}
-                    start={{ x: 0, y: 0 }}
-                    end={{ x: 1, y: 0 }}>
-                    <Text
-                      style={[
-                        tw`w-11/12 text-base`,
-                        colorScheme === 'dark' ? tw`text-white` : tw`text-black`,
-                      ]}>
-                      {taskList?.description}
-                    </Text>
-                    <EvilIcons
-                      name='check'
-                      size={32}
-                      color={colorScheme === 'dark' ? 'white' : 'black'}
-                      onPress={() => completeTask(index)}
-                    />
-                  </LinearGradient>
-                </TouchableOpacity>
-              </View>
-            )
-          })}
-        </ScrollView>
-      ) : (
-        <ScrollView style={tw`px-6 mt-4`}>
-          {taskList.map((taskList, index) => {
-            return (
-              <View key={index}>
-                <TouchableOpacity
-                  onPress={() => editTask(index)}
-                  style={[
-                    tw`bg-white p-2 flex-row rounded-lg items-center mb-4`,
-                    colorScheme === 'dark' && tw`bg-neutral-700`,
-                  ]}>
-                  <Text
-                    style={[
-                      tw`w-11/12 text-base`,
-                      colorScheme === 'dark' ? tw`text-white` : tw`text-black`,
-                    ]}>
-                    {taskList?.description}
-                  </Text>
-                  <EvilIcons
-                    name='check'
-                    size={32}
-                    color={colorScheme === 'dark' ? 'white' : 'black'}
-                    onPress={() => completeTask(index)}
-                  />
-                </TouchableOpacity>
-              </View>
-            )
-          })}
-        </ScrollView>
-      )}
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'position' : 'height'}
-        style={tw`bottom-4`}>
-        <View style={tw`flex-row justify-between px-5`}>
-          <TextInput
-            style={[
-              tw`p-4 mb-4 rounded-full border`,
-              isDisabled ? tw`w-full` : tw`w-4/5`,
-              colorScheme === 'dark'
-                ? tw`bg-neutral-700 text-white border-white`
-                : tw`bg-white text-black`,
-            ]}
-            ref={inputRef}
-            value={task}
-            placeholder={'Write a task'}
-            clearButtonMode='while-editing'
-            onChangeText={(text) => setTask(text)}
-          />
-          {isDisabled ? null : updateIcon ? (
-            <AddTaskButton name='check' onPress={handleUpdateTask} />
-          ) : (
-            <AddTaskButton name='plus' onPress={handleAddTask} />
-          )}
-        </View>
-      </KeyboardAvoidingView>
-    </View>
-  )
+	return (
+		<View style={[tw`flex-1`, colorScheme === 'dark' ? tw`bg-neutral-800` : tw`bg-slate-100`]}>
+			<DrawerToggle />
+			{(stylized as boolean) ? (
+				<ScrollView style={tw`px-6 mt-4`}>
+					{taskList.map((taskList, index) => {
+						return (
+							<View key={index}>
+								<TouchableOpacity onPress={() => editTask(index)} style={tw`justify-between`}>
+									<LinearGradient
+										colors={getColor(index)}
+										style={tw`p-2 flex-row rounded-lg mb-4 items-center`}
+										start={{ x: 0, y: 0 }}
+										end={{ x: 1, y: 0 }}>
+										<Text style={[tw`w-11/12 text-base`, colorScheme === 'dark' ? tw`text-white` : tw`text-black`]}>
+											{taskList?.description}
+										</Text>
+										<EvilIcons
+											name='check'
+											size={32}
+											color={colorScheme === 'dark' ? 'white' : 'black'}
+											onPress={() => completeTask(index)}
+										/>
+									</LinearGradient>
+								</TouchableOpacity>
+							</View>
+						)
+					})}
+				</ScrollView>
+			) : (
+				<ScrollView style={tw`px-6 mt-4`}>
+					{taskList.map((taskList, index) => {
+						return (
+							<View key={index}>
+								<TouchableOpacity
+									onPress={() => editTask(index)}
+									style={[
+										tw`bg-white p-2 flex-row rounded-lg items-center mb-4`,
+										colorScheme === 'dark' && tw`bg-neutral-700`,
+									]}>
+									<Text style={[tw`w-11/12 text-base`, colorScheme === 'dark' ? tw`text-white` : tw`text-black`]}>
+										{taskList?.description}
+									</Text>
+									<EvilIcons
+										name='check'
+										size={32}
+										color={colorScheme === 'dark' ? 'white' : 'black'}
+										onPress={() => completeTask(index)}
+									/>
+								</TouchableOpacity>
+							</View>
+						)
+					})}
+				</ScrollView>
+			)}
+			<KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : 'height'} style={tw`bottom-4`}>
+				<View style={tw`flex-row justify-between px-5`}>
+					<TextInput
+						style={[
+							tw`p-4 mb-4 rounded-full border`,
+							isDisabled ? tw`w-full` : tw`w-4/5`,
+							colorScheme === 'dark' ? tw`bg-neutral-700 text-white border-white` : tw`bg-white text-black`,
+						]}
+						ref={inputRef}
+						value={task}
+						placeholder={'Write a task'}
+						clearButtonMode='while-editing'
+						onChangeText={(text) => setTask(text)}
+					/>
+					{isDisabled ? null : updateIcon ? (
+						<AddTaskButton name='check' onPress={handleUpdateTask} />
+					) : (
+						<AddTaskButton name='plus' onPress={handleAddTask} />
+					)}
+				</View>
+			</KeyboardAvoidingView>
+		</View>
+	)
 }
