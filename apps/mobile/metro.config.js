cd4135bf7feec30178030e7afda7aeb23e71cab3
isaacrmoreno <ipdxcreative@gmail.com>
2022-09-13 15:27:05 -0700

    update indentationt to spaces

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,21 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config')
+const path = require('path')
 
 // Find the workspace root, this can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(__dirname, '../..');
-const projectRoot = __dirname;
+const workspaceRoot = path.resolve(__dirname, '../..')
+const projectRoot = __dirname
 
-const config = getDefaultConfig(projectRoot);
+const config = getDefaultConfig(projectRoot)
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
+config.watchFolders = [workspaceRoot]
 // 2. Resolve module `idb` from file error
-config.resolver.assetExts.push("cjs");
+config.resolver.assetExts.push('cjs')
 // 3. Let Metro know where to resolve packages, and in what order
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
+	path.resolve(projectRoot, 'node_modules'),
+	path.resolve(workspaceRoot, 'node_modules'),
+]
 
-module.exports = config;
+module.exports = config
